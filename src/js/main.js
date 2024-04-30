865befedd47eb462c6912648e80eedd00d649409
orkcel <orkhancelestial@gmail.com>
2024-04-30 11:32:19 +0400

    addec another option

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,14 @@ let nameInput = prompt("Enter your name");
 let ageInput = prompt("Enter your age");
 
 let outputDiv = document.getElementById("inputArea");
-if (ageInput <= 17) {
-    outputDiv.innerHTML = "Cool!!! Your name is " + nameInput + " and you are " + ageInput + " years old. You have awesome future!!!";
+
+if (nameInput === null || ageInput === null || nameInput === "" || ageInput === "") {
+    outputDiv.innerHTML = "Could you please write your name and age correctly?";
+    
 } else {
-    outputDiv.innerHTML = "Cool!!! Your name is " + nameInput + " and you are " + ageInput + " years old. You are not a kid anymore but adult.";
+    if (ageInput <= 17) {
+        outputDiv.innerHTML = "Cool!!! Your name is " + nameInput + " and you are " + ageInput + " years old. You have an awesome future!!!";
+    } else {
+        outputDiv.innerHTML = "Cool!!! Your name is " + nameInput + " and you are " + ageInput + " years old. You are not a kid anymore but an adult.";
+    }
 }
